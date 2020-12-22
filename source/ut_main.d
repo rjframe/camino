3e8ff0a00efc0ea1c57bc89624db88e6f4bdc691
Ryan Frame <code@ryanjframe.com>
2020-12-22 11:51:35 -0500

    Add all modules to unittest main.

--- source/ut_main.d
+++ source/ut_main.d
@@ -1,9 +1,11 @@
 import unit_threaded.runner : runTestsMain;
 
 mixin runTestsMain!(
+    "camino.exception",
     "camino.goal",
     "camino.habit",
     "camino.history",
+    "camino.optparse",
     "camino.schedule",
     "camino.test_util"
 );
